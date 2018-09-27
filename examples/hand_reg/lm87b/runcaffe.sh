fcb2412fdb0a89c6001353050d43f03a18ad3010
zheng.meisong <zheng.meisong@immomo.com>
2018-09-27 17:49:21 +0800

    crop reg with copyMakeBorder

---
+++ examples/hand_reg/lm87b/runcaffe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+set -x
+EXEC_DIR="lm87b"
+export PYTHONPATH=/Users/momo/wkspace/caffe_space/detection/caffe/python:$PYTHONPATH
+export PYTHONPATH=/Users/momo/wkspace/caffe_space/detection/caffe/examples/hand_reg/${EXEC_DIR}:$PYTHONPATH
+TOOLS=./build/tools
+$TOOLS/caffe train \
+  --solver=examples/hand_reg/${EXEC_DIR}/solver.prototxt \
+  --weights=models/lm87b/0926all_iter_60000.caffemodel \
+  2>&1 | tee examples/hand_reg/${EXEC_DIR}/log/`date +'%Y-%m-%d_%H-%M-%S'`.log
