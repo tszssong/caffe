0cf0fefdc5fa4ca6a57827f29c1b382044d64e79
zheng.meisong <zheng.meisong@immomo.com>
2018-10-17 15:35:58 +0800

    reconstruction cropTest

---
+++ examples/s4clsBorder/bbox_transform.py
@@ -0,0 +1,54 @@
+import numpy as np
+import numpy.random as npr
+
+def validBox(box, width, height):
+    rx1, ry1, rx2, ry2 = box
+    if rx1 >= rx2 or ry1 >= ry2:
+        return False
+    rw = rx2 - rx1
+    rh = ry2 - ry1
+    rcx = rx1 + rw / 2
+    rcy = ry1 + rh / 2
+
+    if max(rw, rh) < 60 or rx1 < 0 or ry1 < 0:
+        return False
+
+    if rx1 > rx2 or ry1 > ry2:
+        print ":", x1, y1, x2, y2, "-", width, height
+    if rx2 > width or ry2 > height or rx1 < 0 or ry1 < 0:
+        return False
+
+    return True
+# gt_outside: pix allowed to go outside ground truth box
+# p_ratio: False - crop a square ; True - crop a reactage
+def crop4cls(box, enlarge_bottom, enlargeTop, shift, gt_outside=10, p_ratio=False):
+    # TODO: validBox and enlarge params
+    # newBox = np.array([])
+
+    x1, y1, x2, y2 = box
+    w = x2 - x1
+    h = y2 - y1
+    cx = x1 + w/2
+    cy = y1 + h/2
+    nw = npr.randint(np.ceil(w * enlarge_bottom), np.ceil(w * enlargeTop))
+
+    if p_ratio == False:
+        nh = nw
+    else:
+        nh = int(h*nw/w)
+
+    delta_x = npr.randint(-int(float(w) * shift), int(float(w) * shift + 1))
+    delta_y = npr.randint(-int(float(h) * shift), int(float(h) * shift + 1))
+
+    ncx = cx + delta_x
+    ncy = cy + delta_y
+
+    nx1 = cx - nw / 2
+    ny1 = cy - nh / 2
+    nx2 = cx + nw / 2
+    ny2 = cy + nh / 2
+
+    if nx2 < x2 + gt_outside or nx1 > x1 - gt_outside or ny2 < y2 + gt_outside or ny1 > y1 - gt_outside:
+        return np.array([])
+    else:
+        return np.array([nx1, ny1, nx2, ny2])
--- examples/s4clsBorder/cropTest.py
+++ examples/s4clsBorder/cropTest.py
@@ -5,32 +5,35 @@ import copy
 import os
 import numpy.random as npr
 from utils import IOU, overlapSelf
+from bbox_transform import crop4cls, validBox
+from image_process import  crop_image, fliterDim
 from image_argument import flipAug, rotAug
+
 cropSize = 48
 ScaleS = 1.0
 ScaleB = 2.0
 Shift = 1.5
-RotD = 5
+paddingMode = 'black'
+N_RESIZE = 1
+N_ROT = 1
+date = "_1017"
+
 from_dir = "/Volumes/song/handg_neg_test32G/momoDeepLab4Test/0627all-img/"
 anno_file = "/Users/momo/wkspace/caffe_space/caffe/examples/s4clsBorder/gt/0627-test-all.txt"
 # anno_file = "/Users/momo/wkspace/caffe_space/caffe/examples/s4clsBorder/gt/5-five-wsTest.txt"
 
 # from_dir = "/Volumes/song/handg_neg_test32G/928-zilv-test-img/"
-# anno_file = "/Users/momo/wkspace/caffe_space/caffe/examples/s4clsBorder/gt/5-five-syali1.txt"
+# anno_file = "/Users/momo/wkspace/caffe_space/caffe/examples/s4clsBorder/gt/0928-zilv-test.txt"
 
 to_dir = "/Users/momo/wkspace/caffe_space/caffe/data/48Test/"
-clslists = ['bg', 'heart', 'yearh', 'one', 'baoquan', 'five', 'bainian', 'zan', 'fingerheart', 'ok', \
-            'call', 'rock', 'big_v','fist','palm', 'namaste', 'two_together', 'thumb_down']
+clslists = ['bg', 'heart', 'yearh', 'one', 'baoquan', 'five', 'bainian', 'zan', 'fingerheart', 'ok', 'call', 'rock', 'big_v','fist','palm', 'namaste', 'two_together', 'thumb_down']
+RotDlists = [ 0,       5,       30,    10,         5,    110,         5,    10,            10,   30,     30,     30,      30,    30,     5,         5,              5,     5]
 annofileName = anno_file.split('.')[0].split('/')[-1]
 print annofileName
 
-N_RESIZE = 4
-N_ROT = 2
-date = "_1014"
-
-save_name = annofileName +'_' + str(cropSize)+ 'R'+str(RotD) +'S'+ str(ScaleS).split('.')[0] + str(ScaleS).split('.')[1] + str(int(ScaleB * 10)) + date
-save_dir = save_name + '_1'
-txt_name = save_name + '_1'
+save_name = annofileName +'_' + str(cropSize)+ 'S'+ str(ScaleS).split('.')[0] + str(ScaleS).split('.')[1] + str(int(ScaleB * 10)) + date
+save_dir = save_name
+txt_name = save_name
 
 clsname = annofileName.split('-')[-2]
 print txt_name, save_dir
@@ -40,7 +43,6 @@ if not os.path.exists(to_dir+save_dir):
 fw = open(to_dir + '/Txts/' + txt_name + '.txt', 'w')
 with open(anno_file, 'r') as f:
     annotations = f.readlines()
-
 num = len(annotations)
 
 print "%d pics in total" % num, "NROT:", N_ROT,"N_RESIZE:", N_RESIZE
@@ -54,30 +56,19 @@ for annotation in annotations:
     im_path = annotation[0]
     nbox = int(annotation[1])
 
+    if nbox>=2:
+        continue
     if len(annotation[3:]) > 4:
         annotation.pop(7)
 
     objname = annotation[2]
-    if objname == 'two_together' or clsname == 'thumb_down' or clsname == 'palm' or clsname == 'namaste':
+    if (objname == 'two_together' or objname == 'thumb_down' or objname == 'palm' or objname == 'namaste'):
         continue
-
     cls_idx = clslists.index(objname)
 
-    if nbox>=2:
-        continue
-
-    if cls_idx<9:
-        RotD = 5
-    else:
-        RotD = 25
-    if cls_idx == 2:
-        RotD = 25
-    if cls_idx == 5:
-        RotD = 110
-
+    RotD = RotDlists[cls_idx]
     bbox = map(float, annotation[3:])
     boxes = np.array(bbox, dtype=np.float32).reshape(-1, 4)
-
     image = cv2.imread(os.path.join(from_dir, im_path))
 
     idx += 1
@@ -96,120 +87,46 @@ for annotation in annotations:
     height, width, channel = image.shape
 
     for pic_idx in range(N_ROT):
+
         rot_d = np.random.randint(-RotD, RotD)
-
         img, f_bbox, f_flag = rotAug(image, boxes, rot_d)
-
         f_boxes = np.array(f_bbox, dtype=np.float32).reshape(-1, 4)
-
-        oriH, oriW, oriC = img.shape
         height, width, channel = img.shape
 
         for box_idx in xrange(f_boxes.shape[0]):
-            if f_flag[box_idx] == False:      #skip boxes outside image aftre Rot
+
+            if f_flag[box_idx] == False:      #skip boxes outside image after Rot
                 continue
             box = f_boxes[box_idx]
-            x1, y1, x2, y2 = box
-            rx1, ry1, rx2, ry2 = box
-            if rx1>=rx2 or ry1>=ry2:
+            if validBox(box, width, height) == False:
                 continue
-            
-            w = x2 - x1 + 1
-            h = y2 - y1 + 1
-
-            if max(w, h) < 60 or x1 < 0 or y1 < 0:
-                continue
-            rw = rx2 - rx1
-            rh = ry2 - ry1
-            rcx = rx1 + rw / 2
-            rcy = ry1 + rh / 2
-            if x1 > x2 or y1 > y2:
-                print ":", x1, y1, x2, y2, "-", width, height
-            if x2 > width or y2 > height or x1 < 0 or y1 < 0:
-                continue
-
-            cropped_im = img[int(ry1): int(ry2), int(rx1): int(rx2), :]
-            b = np.mean(cropped_im[:, :, 0])
-            g = np.mean(cropped_im[:, :, 1])
-            r = np.mean(cropped_im[:, :, 2])
-            if ((b - g) < 10 and abs(b - r) < 10):  # blur img
-                continue
-            grey = 0.11 * b + 0.59 * r + 0.3 * g
-            if (grey < 70):  # dark img
+            # cropped_im = img[int(ry1): int(ry2), int(rx1): int(rx2), :]
+            cropped_im = img[int(box[1]): int(box[3]), int(box[0]): int(box[2]), :]
+            if fliterDim(cropped_im) == False:
                 continue
 
             for i in range(N_RESIZE):
-                # maxWH = np.max((rw, rh))
-                # enlargeS = npr.randint(np.ceil(maxWH * ScaleS), np.ceil(ScaleB * maxWH))
-                # delta_x = npr.randint(-float(rw) * Shift, float(rw) * Shift)
-                # delta_y = npr.randint(-float(rh) * Shift, float(rh) * Shift)
-
-                nw = npr.randint(np.ceil(rw * ScaleS), np.ceil(ScaleB * rw))
-                nh = nw
-                if i == 0 and nw < w * 1.5:
-                    delta_x = npr.randint(-int(float(rw) * 0.2), int( float(rw) * 0.2 ) + 1)
-                    delta_y = npr.randint(-int(float(rh) * 0.2), int( float(rh) * 0.2 ) + 1)
-                else:
-                    delta_x = npr.randint(-int(float(rw) * Shift), int(float(rw) * Shift+1))
-                    delta_y = npr.randint(-int(float(rh) * Shift), int(float(rh) * Shift+1))
-
-                ncx = rcx + delta_x
-                ncy = rcy + delta_y
-
-                nx1 = ncx - nw / 2
-                ny1 = ncy - nh / 2
-                nx2 = ncx + nw / 2
-                ny2 = ncy + nh / 2
-
-                if nx2 < rx2 - 10 or nx1 > rx1 + 10 or ny2 < ry2 - 10 or ny1 > ry1 + 10:
+                crop_box = crop4cls(box, ScaleS, ScaleB, Shift,10)
+                if not crop_box.size == 4:
                     continue
-                crop_box = np.array([nx1, ny1, nx2, ny2])
+
                 overlap_flag = 0
                 if nbox > 1:
                     otherboxes = np.array([])
                     for otherbox_idx in xrange(f_boxes.shape[0]):
                         if not box_idx == otherbox_idx:
                             iou = IOU(crop_box, f_boxes[otherbox_idx])
-                            # otherboxes = np.append(otherboxes, f_boxes[otherbox_idx])
                             if iou > 0.01:
                                 overlap_flag = 1
-
                 if overlap_flag == 1:
                     continue
-
-                right_x = 0
-                left_x = 0
-                top_y = 0
-                down_y = 0
-                constant = copy.deepcopy(img)
-                if nx2 > width or ny2 > height or nx1 < 0 or ny1 < 0:
-                    if nx2 > width:
-                        right_x = nx2 - width
-                    if ny2 > height:
-                        down_y = ny2 - height
-                    if nx1 < 0:
-                        left_x = 0 - nx1
-                    if ny1 < 0:
-                        top_y = 0 - ny1
-                    black = [0, 0, 0]
-                    # print "edge:", top_y, down_y, left_x, right_x
-                    constant = cv2.copyMakeBorder(img, int(top_y), int(down_y), int(left_x), int(right_x), cv2.BORDER_CONSTANT, value = black );
-                    # constant = cv2.copyMakeBorder(img, int(top_y), int(down_y), int(left_x), int(right_x), cv2.BORDER_REPLICATE);
-
-                ncropped_im = constant[int(ny1 + top_y):int(ny2 + top_y), int(nx1 + left_x):int(nx2 + left_x), :]
+                ncropped_im = crop_image(img, crop_box, paddingMode)
                 nresized_im = cv2.resize(ncropped_im, (cropSize, cropSize), interpolation=cv2.INTER_NEAREST)
-
-                box_ = box.reshape(1, -1)
-
-                filename = '/'+str(p_idx)+'_'+im_path.split('.')[0]+'.jpg'
-
+                filename = '/'+str(cls_idx)+'_'+str(p_idx)+'_'+im_path.split('.')[0]+'.jpg'
                 save_file = os.path.join(save_dir + filename)
                 fw.write(save_dir + filename + ' ' + str(cls_idx) + '\n')
-
                 cv2.imwrite(to_dir + save_file, nresized_im)
-
                 p_idx += 1
             box_idx += 1
-
 fw.close()
 
