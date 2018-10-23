b1243bd93141f7dac38501c2fe7b927360ad57ec
zheng.meisong <zheng.meisong@immomo.com>
2018-10-23 17:28:39 +0800

    changed mouthmac

--- examples/hand_reg/mouthmac/runcaffe.sh
+++ examples/hand_reg/mouthmac/runcaffe.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 set -x
-EXEC_DIR="mouth"
-export PYTHONPATH=/nfs/zhengmeisong/wkspace/gesture/caffe/python:$PYTHONPATH
-export PYTHONPATH=/nfs/zhengmeisong/wkspace/gesture/caffe/examples/hand_reg/${EXEC_DIR}:$PYTHONPATH
+EXEC_DIR="mouthmac"
+export PYTHONPATH=/Users/momo/wkspace/caffe_space/detection/caffe/python:$PYTHONPATH
+export PYTHONPATH=/Users/momo/wkspace/caffe_space/detection/caffe/examples/hand_reg/${EXEC_DIR}:$PYTHONPATH
 TOOLS=./build/tools
 $TOOLS/caffe train \
   --solver=examples/hand_reg/${EXEC_DIR}/solver.prototxt \
-  --weights=models/mouth/1008f_iter_10000.caffemodel \
+  --weights=models/fromAli/mouth/1023_iter_170000.caffemodel \
   2>&1 | tee examples/hand_reg/${EXEC_DIR}/log/`date +'%Y-%m-%d_%H-%M-%S'`.log
