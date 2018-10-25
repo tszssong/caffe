4ad04c9cfe19fa3acd5f0e5b0293c88f287cc66d
zheng.meisong <zheng.meisong@immomo.com>
2018-10-25 18:00:25 +0800

    add green list

---
+++ examples/s4reg/gt/add_green/cat.sh
@@ -0,0 +1,11 @@
+cat T_2_*.txt > T-2-yeah-total.txt
+cat T_3_*.txt > T-3-one-total.txt
+cat T_7_*.txt > T-7-zan-total.txt
+cat T_8_*.txt > T-8-fheart-total.txt
+cat T_9_*.txt > T-9-ok-total.txt
+cat T_10_*.txt > T-10-call-total.txt
+cat T_11_*.txt > T-11-rock-total.txt
+cat T_12_*.txt > T-12-big_v-total.txt
+cat T_15_*.txt > T-15-palm-total.txt
+cat T_16_*.txt > T-16-thumb_down-total.txt
+
