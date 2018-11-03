5cd72459d6c93f38fd4c3bb9553e90c33eb2400e
zheng.meisong <zheng.meisong@immomo.com>
2018-11-03 10:51:52 +0800

    update

---
+++ examples/hand_reg/128mouth/scp.sh
@@ -0,0 +1,1 @@
+scp -r allen@172.16.9.101:/home/allen/song/$1 ./
--- time.sh
+++ time.sh
@@ -1,5 +1,5 @@
 ./build/tools/caffe time \
---model=mmcv5.prototxt -iterations 3000
+--model=examples/hand_reg/mouthmac/test.prototxt -iterations 3000
 #--model=examples/hand_reg/mouthmac/test.prototxt -iterations 3000
 #--model=/Users/momo/Desktop/sdk/momocv2_model/converted_model/hand_gesture/hand_gesture_reg_v3.0.prototxt -iterations 3000
 #--model=/Users/momo/wkspace/caffe_space/detection/caffe/examples/hand_reg/lm87_64/test.prototxt -iterations 3000
