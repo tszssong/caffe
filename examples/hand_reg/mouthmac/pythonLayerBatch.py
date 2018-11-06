cf3bb7ce7e3b25ef4f2126601255a85b17919882
zheng.meisong <zheng.meisong@immomo.com>
2018-11-06 14:29:26 +0800

    write pickel

--- examples/hand_reg/mouthmac/pythonLayerBatch.py
+++ examples/hand_reg/mouthmac/pythonLayerBatch.py
@@ -7,17 +7,20 @@ import numpy as np
 import random
 import cPickle as pickle
 import time
-imdb_exit = False
+imdb_exit = True
+use_Txt = True
+net_side = 64
 ###############################################################################
 class Data_Layer_test(caffe.Layer):
     def setup(self, bottom, top):
+        self.mean = 128
         self.batch_size = 128
-        net_side = 64
-        roi_list = []
-        roi_root = '/Users/momo/wkspace/caffe_space/detection/caffe/data/1103reg64/'
-        roi_txt='/Users/momo/wkspace/caffe_space/detection/caffe/data/1103reg64/train.txt'
-        print roi_txt
-        self.batch_loader = BatchLoader(roi_list,net_side,roi_root,roi_txt)
+        self.roi_list = []
+        self.roi_root = '/Users/momo/wkspace/caffe_space/detection/caffe/data/1103reg64/testIMDB/'
+        self.roi_txt='/Users/momo/wkspace/caffe_space/detection/caffe/data/1103reg64/testIMDB.txt'
+        print self.roi_txt
+        sys.stdout.flush()
+        self.batch_loader = BatchLoader(self.roi_list,self.roi_root,self.roi_txt)
         top[0].reshape(self.batch_size, 3, net_side, net_side)
         top[1].reshape(self.batch_size, 4)
     def reshape(self, bottom, top):
@@ -25,8 +28,20 @@ class Data_Layer_test(caffe.Layer):
 
     def forward(self, bottom, top):
         loss_task = 1
+        start_time = time.time()
+        ibdb_name = self.batch_loader.load_next_batch(loss_task)
+        fid = open(ibdb_name,'r')
+        self.roi_list = pickle.load(fid)
+        fid.close()
+        print "read a batch use:",time.time() - start_time, " s\n"
+        sys.stdout.flush()
+        
         for itt in range(self.batch_size):
-            im, roi= self.batch_loader.load_next_image(loss_task)
+            im, roi= self.roi_list[itt]
+            im  = np.swapaxes(im, 0, 2)
+            im  = np.swapaxes(im, 1, 2)
+            im  = im.astype(np.int)
+            im -= self.mean
             top[0].data[itt, ...] = im
             top[1].data[itt, ...] = roi
     def backward(self, top, propagate_down, bottom):
@@ -36,13 +51,14 @@ class Data_Layer_test(caffe.Layer):
 ################################################################################
 class Data_Layer_train(caffe.Layer):
     def setup(self, bottom, top):
+        self.mean = 128
         self.batch_size = 128
-        net_side = 64
-        roi_list = ''
-        roi_root = '/Users/momo/wkspace/caffe_space/detection/caffe/data/1103reg64/'
-        roi_txt='/Users/momo/wkspace/caffe_space/detection/caffe/data/1103reg64/train.txt'
-        print roi_txt
-        self.batch_loader = BatchLoader(roi_list,net_side,roi_root,roi_txt)
+        self.roi_list = ''
+        self.roi_root = '/Users/momo/wkspace/caffe_space/detection/caffe/data/1103reg64/trainIMDB/'
+        self.roi_txt='/Users/momo/wkspace/caffe_space/detection/caffe/data/1103reg64/trainIMDB.txt'
+        print self.roi_txt
+        sys.stdout.flush()
+        self.batch_loader = BatchLoader(self.roi_list,self.roi_root,self.roi_txt)
         top[0].reshape(self.batch_size, 3, net_side, net_side)
         top[1].reshape(self.batch_size, 4)
         
@@ -51,38 +67,69 @@ class Data_Layer_train(caffe.Layer):
 
     def forward(self, bottom, top):
         loss_task = 1
-#        start_time = time.time()
+        ibdb_name = self.batch_loader.load_next_batch(loss_task)
+        fid = open(ibdb_name,'r')
+        self.roi_list = pickle.load(fid)
+        fid.close()
+        
         for itt in range(self.batch_size):
-            im, roi= self.batch_loader.load_next_image(loss_task)
+            im, roi= self.roi_list[itt]
+            im  = np.swapaxes(im, 0, 2)
+            im  = np.swapaxes(im, 1, 2)
+            im  = im.astype(np.int)
+            im -= self.mean
             top[0].data[itt, ...] = im
             top[1].data[itt, ...] = roi
-#        print "read a batch use:",time.time() - start_time, " s\n"
-#        sys.stdout.flush()
+
     def backward(self, top, propagate_down, bottom):
         pass
 
 class BatchLoader(object):
-    def __init__(self,roi_list,net_side,roi_root,roi_txt):
+    def __init__(self,roi_list,roi_root,roi_txt):
         self.mean = 128
         self.im_shape = net_side
         self.roi_root = roi_root
         self.roi_list = []
 
-        print "\nStart Reading:",roi_txt," into Memory...\n"
-        fid = open(roi_txt,'r')
-        lines = fid.readlines()
-        fid.close()
-        cur_=0
-        sum_=len(lines)
-        for line in lines:
-            cur_+=1
-            words = line.split()
-            image_file_name = self.roi_root + words[0]
-            roi= [float(words[2]),float(words[3]),float(words[4]),float(words[5])]
-            self.roi_list.append([image_file_name,roi])
-        random.shuffle(self.roi_list)
-        self.roi_cur = 0
-        print "\n",str(len(self.roi_list))," Regression Data have been read into Memory...\n"
+        if imdb_exit:
+            self.roi_cur = 0
+            fid = open(roi_txt,'r')
+            lines = fid.readlines()
+            fid.close()
+            cur_=0
+            sum_=len(lines)
+            for line in lines:
+                cur_+=1
+                words = line.split()
+                imdb_file_name = self.roi_root + words[0]
+                self.roi_list.append(imdb_file_name)
+            print "do not need to read data now"
+        elif use_Txt:
+            fid = open(roi_txt,'r')
+            lines = fid.readlines()
+            fid.close()
+            cur_=0
+            sum_=len(lines)
+            for line in lines:
+                cur_+=1
+                words = line.split()
+                image_file_name = self.roi_root + words[0]
+                roi= [float(words[2]),float(words[3]),float(words[4]),float(words[5])]
+                self.roi_list.append([image_file_name,roi])
+            random.shuffle(self.roi_list)
+            self.roi_cur = 0
+            print "\n",str(len(self.roi_list))," Regression Data have been read into Memory...\n"
+        else:
+            print "not supported on mac"
+
+    def load_next_batch(self,loss_task):
+        if loss_task == 1:
+            if self.roi_cur == len(self.roi_list):
+                self.roi_cur = 0
+                random.shuffle(self.roi_list)
+            batch_name = self.roi_list[self.roi_cur]
+            self.roi_cur += 1
+            return batch_name
 
     def load_next_image(self,loss_task):
         if loss_task == 1:
