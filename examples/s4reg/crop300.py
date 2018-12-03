c3a959b1a8c085674400891d0cdc525eb2ec568b
zheng.meisong <zheng.meisong@immomo.com>
2018-12-03 17:29:45 +0800

    update

--- examples/s4reg/crop300.py
+++ examples/s4reg/crop300.py
@@ -4,26 +4,22 @@ import cv2
 import copy
 import os
 import numpy.random as npr
-from utils import IOU, overlapSelf, overlapingOtherBox
-from bbox_transform import validBox, crop4reg, crop4reg_small
-from image_process import fliterDim, crop_image
+from bbox_transform import validBox, crop4reg_small
+from image_process import crop_image
 paddingMode = 'black'
-OutAllowed = 10   # 10 pixels allowed to go out of gt
 cropSize = 300
 ScaleB = 3.0
 im_dir = "/Users/momo/wkspace/gesture/data/ori/T-5-five_21081025pink-img/"
 anno_file = "/Users/momo/wkspace/caffe_space/detection/caffe/examples/s4reg/gt/T_5_five-pink.txt"
 # im_dir = "/Volumes/song/handg_pink/T-13-fist_20181026pink-img/"
 # anno_file = "/Users/momo/wkspace/caffe_space/detection/caffe/examples/s4reg/gt/T_13_fist-pink.txt"
-
-to_dir = "/Users/momo/wkspace/gesture/data/trainData/"
+to_dir = "../../data/trainData/"
 annofileName = anno_file.split('.')[0].split('/')[-1]
 save_name = annofileName +'_' + str(cropSize)+ 'S'+ str(int(ScaleB * 10))
 save_dir = save_name
 
 if not os.path.exists(to_dir+save_dir):
     os.mkdir(to_dir+save_dir)
-fw = open(os.path.join(to_dir+'Txts/', save_dir+'.txt'), 'w')
 with open(anno_file, 'r') as f:
     annotations = f.readlines()
 num = len(annotations)
@@ -38,7 +34,6 @@ for annotation in annotations:
     nbox = int(annotation[1])
     if nbox>2:
         continue
-
     bbox = map(float, annotation[3:])
     f_boxes = np.array(bbox, dtype=np.float32).reshape(-1, 4)
     image = cv2.imread(os.path.join(im_dir, im_path))
@@ -50,14 +45,10 @@ for annotation in annotations:
     ori_pic_idx += 1
     if ori_pic_idx % 100 == 0:
         print "%s images done, pos: %s"%(ori_pic_idx, croped_pic_idx)
-
     height, width, channel = image.shape
     for box_idx in xrange(f_boxes.shape[0]):
         box = f_boxes[box_idx]
-        # if validBox(box, width, height) == False:
-        #     continue
         crop_box, reg_coord = crop4reg_small(box_idx, f_boxes,  ScaleB)
-        # print crop_box, reg_coord
         if not crop_box.size == 4:
             continue
         ncropped_im = crop_image(image, crop_box, paddingMode)
@@ -67,7 +58,7 @@ for annotation in annotations:
         box_ = box.reshape(1, -1)
         filename = "/" + str(croped_pic_idx) + '_' +  im_path.split('.')[0]
         save_file = os.path.join(save_dir + filename)
-        fw.write(save_dir + filename + '.jpg' + ' 1 %.5f %.5f %.5f %.5f\n' % (reg_coord[0], reg_coord[1], reg_coord[2], reg_coord[3]))
+
         cv2.imwrite(to_dir + save_file + '.jpg', nresized_im)
         annofw = open(to_dir + save_file +'.txt', 'w')
         annofw.write('%.5f %.5f %.5f %.5f\n' % (reg_coord[0]*ratioW, reg_coord[1]*ratioH, reg_coord[2]*ratioW, reg_coord[3]*ratioH) )
@@ -75,5 +66,5 @@ for annotation in annotations:
         croped_pic_idx += 1
         box_idx += 1
 print croped_pic_idx, "images croped"
-fw.close()
+
 
